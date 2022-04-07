29e01116be6eadbfec9c347fc51aebd115372247
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-07 16:54:14 +0300

    correct service

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,30 @@
+from ctypes import BigEndianStructure
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
-def storage_information_view(request):
-    # Программируем здесь
+def format_duration(duration):
+    return str(duration)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+
+
+def storage_information_view(request):
+    active_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in active_visits:
+        duration = format_duration(visit.get_duration())
+        who_entered = visit.passcard.owner_name
+        entered_at = visit.entered_at
+        is_strange = visit.is_visit_long()
+        non_closed_visits.append(
+            {
+                'who_entered': who_entered,
+                'entered_at': entered_at,
+                'duration': duration,
+                'is_strange': is_strange,
+            },
+        )
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
