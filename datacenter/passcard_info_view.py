8852b0920c1d7ad48c745a5fe268c6aadf85de6a
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-19 15:55:42 +0300

    rm unused function

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,17 +1,15 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from datacenter.storage_information_view import format_duration
-from .storage_information_view import format_duration
 
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
-    visits = Visit.objects.filter(passcard=passcard)
+    serialized_visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = []
-    for visit in visits:
+    for visit in serialized_visits:
         entered_at = visit.entered_at
-        duration = format_duration(visit.get_duration())
+        duration = visit.get_duration()
         is_strange = visit.is_visit_long()
         this_passcard_visits.append(
             {
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,16 +4,11 @@ from datacenter.models import Visit
 from django.shortcuts import render
 
 
-def format_duration(duration):
-    return str(duration)
-
-
-
 def storage_information_view(request):
     active_visits = Visit.objects.filter(leaved_at=None)
     non_closed_visits = []
     for visit in active_visits:
-        duration = format_duration(visit.get_duration())
+        duration = visit.get_duration()
         who_entered = visit.passcard.owner_name
         entered_at = visit.entered_at
         is_strange = visit.is_visit_long()
@@ -26,6 +21,6 @@ def storage_information_view(request):
             },
         )
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
