9836331e7e9158cdbc578f8ae050701ad1879fdb
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-22 08:33:05 +0300

    correct var names

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 
 def active_passcards_view(request):
     active_passcards = Passcard.objects.filter(is_active=True)
-    visits = Visit.objects.all()
     context = {
         'active_passcards': active_passcards,
     }
