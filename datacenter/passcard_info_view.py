c0fb6c981cc4273892b1d01290dfcc9f4463cf3e
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-22 08:33:05 +0300

    correct var names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
-    serialized_visits = Visit.objects.filter(passcard=passcard)
+    deserialized_visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = []
-    for visit in serialized_visits:
+    for visit in deserialized_visits:
         entered_at = visit.entered_at
         duration = visit.get_duration()
         is_strange = visit.is_visit_long()
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,13 +6,13 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     active_visits = Visit.objects.filter(leaved_at=None)
-    non_closed_visits = []
+    active_serialized_visits = []
     for visit in active_visits:
         duration = visit.get_duration()
         who_entered = visit.passcard.owner_name
         entered_at = visit.entered_at
         is_strange = visit.is_visit_long()
-        non_closed_visits.append(
+        active_serialized_visits.append(
             {
                 'who_entered': who_entered,
                 'entered_at': entered_at,
@@ -21,6 +21,6 @@ def storage_information_view(request):
             },
         )
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': active_serialized_visits,
     }
     return render(request, 'storage_information.html', context)
